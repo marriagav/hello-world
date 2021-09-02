536315b1fbd2c036a2d09a7f76114848502a4f33
marriagav <65412950+marriagav@users.noreply.github.com>
2021-09-01 21:29:51 -0500

    Delete Video database project directory

--- Video database project/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "programa.h"
-#include <string>
-
-int main(){
-    Programa programa2=Programa("datosPeliculas.txt","datosSeries.txt","datosCapitulos.txt");
-    programa2.menu();
-    
-    return 0;
-};
